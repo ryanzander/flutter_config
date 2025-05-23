a116790b4d52b5d45a14c86d78f93ef4a01a1154
Ryan <rjameszander@gmail.com>
2025-05-23 20:45:18 +0700

    Remove Registrar; migrate to Flutter plugin API v2

--- android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
+++ android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.lang.IllegalArgumentException
 import java.lang.reflect.Field
 
@@ -29,14 +28,6 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
     applicationContext = null
-  }
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_config")
-      channel.setMethodCallHandler(FlutterConfigPlugin(registrar.activity()))
-    }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
